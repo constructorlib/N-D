ee4f84ec84962344c3d3f28bd48d0f1da1583c31
constructorlib <constructorlib@gmail.com>
2023-03-06 15:34:24 +0900

    round_up

--- misc/index.js
+++ misc/index.js
@@ -1,6 +1,10 @@
 // const isNull = document.getElementById("height").value;
-function alertNull(){
-    let isNull = document.getElementById("height").value;
-
-    alert(isNull);
+function round_up(){
+    let height = document.getElementById("height").value;
+    height *= height;
+    let weight = document.getElementById("weight").value;
+    weight /= height;
+    // alert(Math.round(weight));
+    let weight_round = Math.round(weight);
+    return weight_round;
 }
