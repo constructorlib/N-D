30a83a4b76da6c3e4e31d952b409ffed712bdcdb
constructorlib <constructorlib@gmail.com>
2023-03-08 14:58:13 +0900

    Less html, more innerHTML

--- misc/index.js
+++ misc/index.js
@@ -1,29 +1,26 @@
-var underweight = document.getElementsByClassName("underweight");
-var normal = document.getElementsByClassName("normal");
-console.log(normal);
+var bmiweight = document.getElementsByClassName("bmiweight");
+// var normal = document.getElementsByClassName("normal");
+// console.log(normal);
 
 
 function round_up(){
     let height = document.getElementById("height").value;
-    height = parseInt(height)
+    height = parseInt(height);
     height *= height;
     let weight = document.getElementById("weight").value;
     weight = parseInt(weight);
-    weight = height/weight;
+    weight = weight/height;
+    console.log(weight);
     let weight_round = Math.round(weight);
-    console.log(weight_round);
-    // return weight_round;
-    console.log(weight_round);
+    // console.log(weight_round);
     if(weight_round <= 18){
-        for (let i = 0; i < underweight.length; i++) {
-            underweight[i].style.visibility = "visible";
+        for (let i = 0; i < bmiweight.length; i++) {
+            bmiweight[i].style.visibility = "visible";
         }
+        bmiweight[0].innerHTML = "12313";
+
+
     } 
-    else if(weight_round > 18 ){
-        for (let i = 0; i < normal.length; i++) {
-            normal[i].style.display = "flex";
-        }
-    }
 }
 document.addEventListener("DOMContentLoaded", function() {
     var button = document.getElementById("btn-submit");
