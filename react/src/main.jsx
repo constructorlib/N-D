9bd360f265ee9ec13450aaf69838e37149f94766
constructorlib <constructorlib@gmail.com>
2023-04-19 03:41:23 +0900

    bootstrap

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import App from './App'
 import ReactDom from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function RootComponent(){
   return (
@@ -14,16 +15,20 @@ function RootComponent(){
 }
 
 const Book = () => {
-  return <article>
-    <Image />
-    <Title />
-    <Author />
-  </article>
+  return (
+    <article className='booklist'>
+      <BookSection />
+    </article>
+  );
 }
-const Title = () => <h1>I Love you to the Moon and Back</h1>
 
-const Author =() => <h4>Dinora Khoshimova</h4>
-const Image = () => 
-<img src='https://m.media-amazon.com/images/I/8144Vic9C5L._AC_UF1000,1000_QL80_.jpg' 
-width='250px' alt='' />
+const BookSection = () => (
+  <div className='container'>
+    <h5>I love you to the Moon and back</h5>
+    <img src='https://m.media-amazon.com/images/I/8144Vic9C5L._AC_UF1000,1000_QL80_.jpg' 
+    width='250px' alt='' />
+    <br /> <br />
+    <p> <i>By</i> Dinora Khoshimova</p>
+  </div>
+);
 ReactDom.render(<RootComponent />, document.getElementById('root'));
