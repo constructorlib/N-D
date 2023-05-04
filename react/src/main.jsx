e9acc992e6779e43e51e07c24fe17b8c023d0082
constructorlib <constructorlib@gmail.com>
2023-05-04 23:53:15 +0900

    done, thanks gtp

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,41 +1,76 @@
-import React from "react";
-import App from "./App";
-import ReactDom from "react-dom";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BookDisplay } from "./bookDisplay";
-import BookSection from "./BookSection";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import { faVolumeUp } from "@fortawesome/free-solid-svg-icons";
-// var invoke
-const url =
-  "https://www.dictionaryapi.com/api/v3/references/collegiate/json/voluminous?key=your-api-key";
+
 function RootComponent() {
+  const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+  const [word, setWord] = useState("");
+  const [meaning, setMeaning] = useState("");
+  const [example, setExample] = useState("");
+  const [partOfSpeech, setPartOfSpeech] = useState("");
+  const [phonetic, setPhonetic] = useState("");
+  const [audio, setAudio] = useState("");
+
+  const handleSearch = () => {
+    fetch(`${url}${word}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setMeaning(data[0].meanings[0].definitions[0].definition);
+        setExample(data[0].meanings[0].definitions[0].example || "");
+        setPartOfSpeech(data[0].meanings[0].partOfSpeech);
+        setPhonetic(data[0].phonetics[0].text);
+        setAudio(data[0].phonetics[0].audio);
+      })
+      .catch(() => {
+        setMeaning("Couldn't find the word");
+        setExample("");
+        setPartOfSpeech("");
+        setPhonetic("");
+        setAudio("");
+      });
+  };
+
+  const playSound = () => {
+    const audioElement = document.getElementById("audio");
+    audioElement.setAttribute("src", `https:${audio}`);
+    audioElement.play();
+  };
+
   return (
     <div className="loot">
+      <audio id="audio"></audio>
       <div className="ctn">
         <input
           type="text"
-          placeholder="Type the word here... "
-          id="word-input"
+          placeholder="Type the word here..."
+          value={word}
+          onChange={(e) => setWord(e.target.value)}
         />
-        <button id="search-btn ">Search</button>
+        <button onClick={handleSearch}>Search</button>
       </div>
       <div className="result">
         <div className="sample">
-          <h3>Sample OLD MAN</h3>
-          <button>
-            <i className="fas fa-volume-up"></i>
+          <h3>{word}</h3>
+          <button onClick={playSound}>
+            <FontAwesomeIcon icon={faVolumeUp} />
           </button>
         </div>
         <div className="details">
-          <p>pos</p>
-          <p>/sample/</p>
+          <p>{partOfSpeech}</p>
+          <p>/ {phonetic} /</p>
         </div>
+        <p className="word-definition">{meaning}</p>
+        {example && <p className="word-example">{example}</p>}
       </div>
     </div>
   );
 }
-// navbar
+
 const NavBar = () => (
   <nav className="navbar bg-secondary">
     <div className="container-fluid">
@@ -43,24 +78,5 @@ const NavBar = () => (
     </div>
   </nav>
 );
-// book
 
-ReactDom.render(<RootComponent />, document.getElementById("root"));
-
-/*
-<>
-      <section>
-        <NavBar />
-      </section>
-      <br />
-      <div className="container">
-        <div className="row">
-          <p></p>
-          {BookDisplay.map((book) => {
-            return <BookSection params={book} />;
-          })}
-        </div>
-      </div>
-    </>
-
-*/
+ReactDOM.render(<RootComponent />, document.getElementById("root"));
